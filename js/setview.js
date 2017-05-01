bbc783ccf4fe3e73a203bb4be0b470bdc385d977
ethankulman <ethan@Ethans-MacBook-Pro.local>
2017-04-30 21:29:01 -0400

    added jquery to setview and fixed some front end

--- js/setview.js
+++ js/setview.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    $(".difficulty-icon").tooltip();
     $('.questionInfo').each(function(){
     var number = $(this).find("#question-difficulty");
     var difficulty = number.text();
@@ -15,13 +16,20 @@ $(document).ready(function() {
 
     var statsDiv = $("#stats-side-div");
     $(".difficulty-icon").click(function(e) {
+        $(".question-view-format").each(function(){
+            if($(this).parent().is("a")){
+                $(this).unwrap();
+            }
+        });
+        var question = $(this).closest(".questionInfo");
+        question.find(".question-view-format").wrap('<a href="#"><a/>')
         var correctAns = $(this).find("#question-correct-answer").text();
         var correctAtt = $(this).find("#question-correct-attempts").text();
         var totalAtt = $(this).find("#question-total-attempts").text();
         statsDiv.html('<div class="center"><div><div class="side-bar-title">Correct Answer</div>' + correctAns + '</div><br>'
                          + '<div><div class="side-bar-title">Total Attempts</div>' + totalAtt + '</div></div>');
 
-
+        e.preventDefault();
 
     });
 });
